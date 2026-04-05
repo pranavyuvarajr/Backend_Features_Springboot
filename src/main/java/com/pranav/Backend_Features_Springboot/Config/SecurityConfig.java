d17cbe9e8d321df4c7bb1b56ebf41fdd3fe34e10
pranavyuvarajr <pranavyuvarajr@gmail.com>
2026-04-05 17:15:06 +0530

    spring security config

---
+++ src/main/java/com/pranav/Backend_Features_Springboot/Config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.pranav.Backend_Features_Springboot.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .cors(c -> c.disable())
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.disable())
+                .build();
+    }
+}
