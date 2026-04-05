0d40822dc949e29ca8ad0aeae5ac1e449dc0b4d7
pranavyuvarajr <pranavyuvarajr@gmail.com>
2026-04-05 11:06:04 +0530

    first commit

---
+++ src/main/java/com/pranav/Backend_Features_Springboot/BackendFeaturesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pranav.Backend_Features_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFeaturesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFeaturesSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pranav/Backend_Features_Springboot/BackendFeaturesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.Backend_Features_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFeaturesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
