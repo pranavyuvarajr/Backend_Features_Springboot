fbb4f9463be28207fd39eb049e73355cc8e228cd
pranavyuvarajr <pranavyuvarajr@gmail.com>
2026-04-06 21:18:01 +0530

    login and register endpoint desided to add

---
+++ src/main/java/com/pranav/Backend_Features_Springboot/Controller/User.java
@@ -0,0 +1,13 @@
+package com.pranav.Backend_Features_Springboot.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class User {
+    @PostMapping("/register")
+    public String addUser(@RequestMapping )
+}
